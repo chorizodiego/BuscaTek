6d89d270aed08edca6a3491fbf0ad1cb746ba341
Tomás Gómez Molina <tomasgomezmolina@MacBook-Pro-de-Tomas.local>
2017-05-01 14:35:34 -0300

    adding Server 2

---
+++ Server/README.md
@@ -0,0 +1,6 @@
+Para levantar el servidor necesita:
+
+	- Correr mongod (base de datos)
+		mongod
+	- Correr servidor
+		npm start
---
+++ Server/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+	//'url' : 'mongodb://<dbuser>:<dbpassword>@ds127531.mlab.com:27531/buscatek'
+	'url' : 'mongodb://localhost/test'
+}
